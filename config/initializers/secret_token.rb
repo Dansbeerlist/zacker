b86788f29464adaa7698ffa6ee5b2c6f10688dca
Dansbeerlist <dansbeerlist@gmail.com>
2012-10-22 18:22:31 -0700

    added files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zackapack::Application.config.secret_token = 'a27b074ca7c599aebe50d45e6c6c26c284a98beb44d5b8684caa4ec120d149383226df45c716c6d56f11211f25626452a36bc42f54e9c9c96f0ccbec2e7e5a08'
---
+++ test/functional/lessons_controller_test.rb
@@ -0,0 +1,54 @@
+require 'test_helper'
+
+class LessonsControllerTest < ActionController::TestCase
+  test "should get one" do
+    get :one
+    assert_response :success
+  end
+
+  test "should get two" do
+    get :two
+    assert_response :success
+  end
+
+  test "should get three" do
+    get :three
+    assert_response :success
+  end
+
+  test "should get four" do
+    get :four
+    assert_response :success
+  end
+
+  test "should get five" do
+    get :five
+    assert_response :success
+  end
+
+  test "should get six" do
+    get :six
+    assert_response :success
+  end
+
+  test "should get seven" do
+    get :seven
+    assert_response :success
+  end
+
+  test "should get eight" do
+    get :eight
+    assert_response :success
+  end
+
+  test "should get nine" do
+    get :nine
+    assert_response :success
+  end
+
+  test "should get ten" do
+    get :ten
+    assert_response :success
+  end
+
+end
